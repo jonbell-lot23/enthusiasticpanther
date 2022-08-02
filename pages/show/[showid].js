2fc4b68cc98341a6cba8a364acc6813f97c733ac
Jon Bell <github@lot23.com>
2022-08-02 17:28:56 +1200

    Dynamic?

--- pages/show/[showid].js
+++ pages/show/[showid].js
@@ -4,52 +4,35 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function Home() {
-  const [show, setshow] = useState();
-  const router = useRouter();
-  const { showid } = router.query; // Destructuring our router object
+function Page({ data }) {
   
+  console.log("loading...");
+  console.log(data);
   
-  // Function to collect data
-  const getApiData = async () => {
-    const showResponse = await fetch(      
-      "https://lot23.com/play/enthusiasticpanther/json/show.php?showid=" + showid
-    ).then((response) => response.json());      
-    
-    setshow(showResponse);
-    console.log(showResponse);
-  };
-  
-  useEffect(() => {
-    getApiData();
-  }, []);
-  
+  // Render data...
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Enthusiastic Panther is a made-up band</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <header>enthusiastic panther, your favourite made-up band from New Zealand</header>                                    
-      <div className="mt-4">
-        
-        <p>
-        Show #{showid} had these songs:
-        </p>
-        
-        {show &&
-        show.map((song) => (
-          <span>
-          {song.name}{" "}
-          </span>
-        ))}
-        
-      </div>
-                         
-                         
-                          
-
-    </div>
+  
+  <div>hi</div>
+  
   );
 }
+
+// This gets called on every request
+export async function getServerSideProps() {
+  
+  // const router = useRouter();
+  // const { showid } = router.query; // Destructuring our router object
+  
+  
+  
+  // Fetch data from external API
+  const res = await fetch("https://lot23.com/play/enthusiasticpanther/json/show.php?showid=100");
+  const data = await res.json()
+
+  console.log("loading...");
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+export default Page
