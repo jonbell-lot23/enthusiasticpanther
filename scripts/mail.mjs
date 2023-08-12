8f19d09765fed2b5e2b972c5cb534bfdd069b6b8
Jon Bell <github@lot23.com>
2023-08-12 22:21:23 +1200

    New script

--- scripts/mail.mjs
+++ scripts/mail.mjs
@@ -2,9 +2,14 @@ import { Resend } from "resend";
 
 const resend = new Resend("re_7nsaGUMW_KVKi5vMNmwXgFWxS7QftNfph");
 
-resend.emails.send({
-  from: "onboarding@resend.dev",
-  to: "jb@lot23.com",
-  subject: "🎸 EP email",
-  html: "<p>This is an automated message from Enthusiastic Panther</p>",
-});
+resend.emails
+  .send({
+    from: "onboarding@resend.dev",
+    to: "jb@lot23.com",
+    subject: "🎸 EP email",
+    html: "<p>This is an automated message from Enthusiastic Panther</p>",
+  })
+  .catch((err) => {
+    console.error("An error occurred:", err);
+    process.exit(1);
+  });
