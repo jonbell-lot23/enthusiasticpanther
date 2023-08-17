0f387624d57c5978b9a6abb51edf5fdce077edcb
Jon Bell <github@lot23.com>
2023-08-17 12:45:59 +1200

    Quilts

--- pages/api/checkImage.js
+++ pages/api/checkImage.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const showId = parseInt(req.query.showId, 10);
 
-  if (showId <= 57) {
+  if (showId <= 70) {
     res.status(200).json({ exists: true });
   } else {
     res.status(200).json({ exists: false });
