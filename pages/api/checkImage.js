348edb9d410115b9b4eef81dd8493b71772cd73f
Jon Bell <github@lot23.com>
2023-08-17 20:13:18 +1200

    New images

--- pages/api/checkImage.js
+++ pages/api/checkImage.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const showId = parseInt(req.query.showId, 10);
 
-  if (showId <= 73 || (showId >= 213 && showId <= 219)) {
+  if (showId <= 73 || (showId >= 211 && showId <= 219)) {
     res.status(200).json({ exists: true });
   } else {
     res.status(200).json({ exists: false });
