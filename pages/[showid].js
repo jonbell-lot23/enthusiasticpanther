948c2b280e1d8f57f4c1faa22241d9b68fe9c1e5
Jon Bell <github@lot23.com>
2023-08-19 22:11:13 +1200

    Songs

---
+++ pages/[showid].js
@@ -0,0 +1,81 @@
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+import { PrismaClient } from "@prisma/client";
+import React, { useEffect, useRef } from "react";
+import ShowCard from "../../components/ShowCard";
+
+function Song(props) {
+  return (
+    <div className={styles.songContainer}>
+      <div className={styles.songTitle}>{props.name}</div>
+      <div className={styles.songQuality}>{props.quality}</div>
+    </div>
+  );
+}
+
+function Page({ data, showId, location }) {
+  return (
+    <div className={styles.container}>
+      <div className="container">
+        <Head>
+          <title>Enthusiastic Shows</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className="flex my-4">
+          <div className={styles.albumContainer}>
+            <ShowCard showId={showId} location={location} />
+            <div className={styles.metadata}>
+              <div className={styles.metadataTitle}></div>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <main className="mb-12">
+              <div className="app">
+                {data &&
+                  data.map((song) => (
+                    <Song name={song.name} quality={song.quality} />
+                  ))}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const prisma = new PrismaClient();
+  const showId = Number(context.query.showid);
+
+  const showDetails = await prisma.ep_shows.findUnique({
+    where: { id: showId },
+  });
+
+  // Fetch the song performances for the given show ID
+  const performances = await prisma.ep_songperformances.findMany({
+    where: { showid: showId },
+  });
+
+  // Map the performances into the desired format for the page
+  const data = await Promise.all(
+    performances.map(async (performance) => {
+      // Fetch song details for each performance
+      const song = await prisma.ep_songs.findUnique({
+        where: { id: performance.songid },
+      });
+
+      return {
+        name: song.name,
+        quality: performance.quality,
+      };
+    })
+  );
+
+  await prisma.$disconnect();
+
+  return { props: { data, showId, location: showDetails.location } };
+}
+
+export default Page;
---
+++ pages/song/[songid].js
@@ -0,0 +1,78 @@
+import Head from "next/head";
+import styles from "../../styles/Home.module.css"; // Update stylesheet for song details
+import { PrismaClient } from "@prisma/client";
+
+function Performance(props) {
+  return (
+    <div className={styles.performanceContainer}>
+      <div className={styles.showLocation}>{props.location}</div>
+      <div className={styles.showDate}>{props.date}</div>
+      <div className={styles.showQuality}>{props.quality}</div>
+    </div>
+  );
+}
+
+function SongPage({ songDetails, performances }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{`Enthusiastic Shows - ${songDetails.name}`}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className={styles.songTitle}>{songDetails.name}</h1>
+      <div className={styles.songDetails}>
+        {/* Additional song details can be displayed here */}
+      </div>
+
+      <h2 className={styles.performancesTitle}>Performances</h2>
+      <div className={styles.performancesContainer}>
+        {performances &&
+          performances.map((performance) => (
+            <Performance
+              location={performance.location}
+              date={performance.date}
+              quality={performance.quality}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const prisma = new PrismaClient();
+  const songId = Number(context.query.songid);
+
+  // Fetch the details for the given song ID
+  const songDetails = await prisma.ep_songs.findUnique({
+    where: { id: songId },
+  });
+
+  // Fetch the song performances for the given song ID
+  const songPerformances = await prisma.ep_songperformances.findMany({
+    where: { songid: songId },
+  });
+
+  // Map the song performances to include show details
+  const performances = await Promise.all(
+    songPerformances.map(async (performance) => {
+      // Fetch show details for each performance
+      const show = await prisma.ep_shows.findUnique({
+        where: { id: performance.showid },
+      });
+
+      return {
+        location: show.location,
+        date: show.date,
+        quality: performance.quality,
+      };
+    })
+  );
+
+  await prisma.$disconnect();
+
+  return { props: { songDetails, performances } };
+}
+
+export default SongPage;
