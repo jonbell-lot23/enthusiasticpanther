714f996e478bfdbbea46ba36536a25f5bc180dde
Jon Bell <github@lot23.com>
2023-08-26 18:14:08 +1200

    Adding song pages

--- pages/song/[songid].js
+++ pages/song/[songid].js
@@ -1,37 +1,25 @@
-import Head from "next/head";
-import styles from "../../styles/Home.module.css"; // Update stylesheet for song details
+import styles from "./Song.module.css"; // Update stylesheet for song details
 import { PrismaClient } from "@prisma/client";
-
-function Performance(props) {
-  return (
-    <div className={styles.performanceContainer}>
-      <div className={styles.showLocation}>{props.location}</div>
-      <div className={styles.showDate}>{props.date}</div>
-      <div className={styles.showQuality}>{props.quality}</div>
-    </div>
-  );
-}
+import ShowCard from "../../components/ShowCard";
 
 function SongPage({ songDetails, performances }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>{`Enthusiastic Shows - ${songDetails.name}`}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <h1 className={styles.songTitle}>{songDetails.name}</h1>
-      <div className={styles.songDetails}></div>
-
-      <h2 className={styles.performancesTitle}>Performances</h2>
       <div className={styles.performancesContainer}>
         {performances &&
           performances.map((performance) => (
-            <Performance
-              location={performance.location}
-              date={performance.date}
-              quality={performance.quality}
-            />
+            <div key={performance.id} className={styles.card}>
+              <ShowCard
+                showId={performance.showId}
+                location={performance.location}
+                showScore={performance.quality}
+              />
+
+              {performance.isDebut && (
+                <p className="text-xs">{performance.quality}</p>
+              )}
+            </div>
           ))}
       </div>
     </div>
@@ -50,6 +38,7 @@ export async function getServerSideProps(context) {
   // Fetch the song performances for the given song ID
   const songPerformances = await prisma.ep_songperformances.findMany({
     where: { songid: songId },
+    orderBy: { id: "desc" },
   });
 
   // Map the song performances to include show details
@@ -64,6 +53,7 @@ export async function getServerSideProps(context) {
         location: show.location,
         date: show.date,
         quality: performance.quality,
+        showId: performance.showid,
       };
     })
   );
