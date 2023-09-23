7510016f9c8b02580c78391c33ffeafd1702a901
Jon Bell <github@lot23.com>
2023-09-23 13:36:47 +1200

    Added a test! I am awesome.

---
+++ cypress/e2e/1-successful-load.cy.ts
@@ -0,0 +1,28 @@
+describe('Enthusiastic Panther Website Navigation', () => {
+  it('should navigate through header items and ensure they load correctly', () => {
+    // Go to the home page
+    cy.visit('https://enthusiasticpanther.com/');
+
+    // Verify that the home page has loaded
+    cy.contains('Latest shows');
+    cy.contains('Highly rated shows');
+
+    // Click "About" in the header and verify it loads
+    cy.get('div.flex > div.space-x-4 > a[href="/about"]').click();
+    cy.url().should('include', '/about');
+    // Add a check for some content that should appear on the "About" page
+    // cy.contains('Some content specific to About page');
+
+    // Click "Shows" in the header and verify it loads
+    cy.get('div.flex > div.space-x-4 > a[href="/shows"]').click();
+    cy.url().should('include', '/shows');
+    // Add a check for some content that should appear on the "Shows" page
+    // cy.contains('Some content specific to Shows page');
+
+    // Click "Songs" in the header and verify it loads
+    cy.get('div.flex > div.space-x-4 > a[href="/songs"]').click();
+    cy.url().should('include', '/songs');
+    // Add a check for some content that should appear on the "Songs" page
+    // cy.contains('Some content specific to Songs page');
+  });
+});
