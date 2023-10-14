92884729c691d0686ef08030c67b55fd2596a76d
Jon Bell <github@lot23.com>
2023-10-14 13:55:50 +1300

    Hard-code production

--- e2e/1-successful-load.spec.ts
+++ e2e/1-successful-load.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const BASE_URL = isProduction ? 'https://enthusiasticpanther.com' : 'http://localhost:3000';
+const BASE_URL = isProduction ? 'https://enthusiasticpanther.com' : 'https://enthusiasticpanther.com';
 
 test('test', async ({ page }) => {
   await page.goto(BASE_URL);
