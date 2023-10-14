2093a4a7f12e555525427213a73bd225d2a688bf
Jon Bell <github@lot23.com>
2023-10-14 13:09:36 +1300

    Production push

--- e2e/1-successful-load.spec.ts
+++ e2e/1-successful-load.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const BASE_URL = isProduction ? 'https://enthusiasticpanther.com' : 'http://localhost:3000';
+
 test('test', async ({ page }) => {
+  await page.goto(BASE_URL);
   await page.goto('http://localhost:3000/');
   await page.waitForSelector('text="Latest shows"');
   await page.waitForSelector('text="Highly rated shows"');
