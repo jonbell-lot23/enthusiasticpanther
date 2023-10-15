1b31cea5e99b8ad7df826abe2b669be5f9c49df3
Jon Bell <github@lot23.com>
2023-10-15 19:22:01 +1300

    Woo, I added historgram graphs

--- pages/histogram/[showid].js
+++ pages/histogram/[showid].js
@@ -1,37 +1,36 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 import Subnav from "../../components/Subnav";
+import { Bar } from "react-chartjs-2";
+import "chart.js/auto";
+const prisma = new PrismaClient();
 
 const HistogramPage = ({ songsData, showId }) => {
   const eras = [
-    { era: 1, start: 0, end: 14 },
-    { era: 2, start: 15, end: 26 }, // the missoula show
-    { era: 3, start: 27, end: 53 }, // the halifax show
-    { era: 4, start: 54, end: 57 }, // the tel-aviv show
-    { era: 5, start: 58, end: 59 }, // the kyoto show
-    { era: 6, start: 60, end: 101 }, // the tokyo show
-    { era: 7, start: 102, end: 121 }, // the el paso show
-    { era: 8, start: 122, end: 182 }, // the winston-salem show
-    { era: 9, start: 183, end: 232 }, // the charlotte show
-    { era: 10, start: 233, end: 300 }, // the new york show
+    { era: 1, start: 0, end: 14, name: "Early Days" },
+    { era: 2, start: 15, end: 26, name: "Missoula" },
+    { era: 3, start: 27, end: 53, name: "Halifax" },
+    { era: 4, start: 54, end: 57, name: "Tel-Aviv" },
+    { era: 5, start: 58, end: 59, name: "Kyoto" },
+    { era: 6, start: 60, end: 101, name: "Tokyo" },
+    { era: 7, start: 102, end: 121, name: "El Paso" },
+    { era: 8, start: 122, end: 182, name: "Winston-Salem" },
+    { era: 9, start: 183, end: 232, name: "Charlotte" },
+    { era: 10, start: 233, end: 300, name: "New York" },
   ];
-
-  console.log("Props received in HistogramPage:", { songsData, showId });
-
-  if (!songsData) {
-    return <div>Error: Song data is not available.</div>;
-  }
 
   const groupedByEra = eras.reduce((acc, era) => {
     acc[era.era] = { count: 0, shows: [], debutDates: [] };
     return acc;
   }, {});
 
+  if (!songsData) {
+    return <div>Error: Song data is not available.</div>;
+  }
+
   songsData.forEach((song) => {
     const era = eras.find(
-      (era) => song.debutShow >= era.start && song.debutShow <= era.end
+      (e) => song.debutShow >= e.start && song.debutShow <= e.end
     );
     if (era) {
       groupedByEra[era.era].count += 1;
@@ -40,12 +39,48 @@ const HistogramPage = ({ songsData, showId }) => {
     }
   });
 
+  const eraLabels = eras.map((e) => e.name);
+  const eraCounts = eras.map((e) => groupedByEra[e.era].count);
+
+  const chartData = {
+    labels: eraLabels,
+    datasets: [
+      {
+        label: "Number of Songs",
+        data: eraCounts,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const verticalLine = {
+    type: "line",
+    label: "Current Show",
+    borderColor: "#FF0000",
+    borderWidth: 2,
+    fill: false,
+    data: [],
+    pointRadius: 0,
+  };
+
+  const currentEraIndex = eras.findIndex(
+    (era) => showId >= era.start && showId <= era.end
+  );
+  if (currentEraIndex !== -1) {
+    verticalLine.data = Array(eraLabels.length).fill(null);
+    verticalLine.data[currentEraIndex] = Math.max(...eraCounts);
+  }
+  chartData.datasets.push(verticalLine);
+
   return (
     <div>
       <Subnav showId={showId} />
       <div className="w-full max-w-2xl p-4 mx-auto prose prose-lg bg-white rounded-lg shadow-lg">
+        <Bar data={chartData} />
         {eras.map((era) => (
-          <div key={era.era}>
+          <div key={era.era} className="hidden">
             Era {era.era}: {groupedByEra[era.era].count} (
             {groupedByEra[era.era].debutDates.join(", ")})
           </div>
@@ -58,8 +93,6 @@ const HistogramPage = ({ songsData, showId }) => {
 export async function getServerSideProps(context) {
   try {
     const showId = Number(context.params.showid);
-    console.log("Show ID from params:", context.params.showid);
-    console.log("Converted Show ID:", showId);
 
     const performances = await prisma.ep_songperformances.findMany({
       where: { showid: showId },
@@ -76,13 +109,10 @@ export async function getServerSideProps(context) {
           where: { id: performance.songid },
         });
 
-        console.log("Fetched song:", song);
-
-        // Assume an empty album if not provided
         const album = song?.album || "";
 
         const previousPerformances = await prisma.ep_songperformances.findMany({
-          where: { songid: performance.songid, showid: { lt: showId } }, // fetch performances before the current show
+          where: { songid: performance.songid, showid: { lt: showId } },
           orderBy: { showid: "asc" },
         });
 
@@ -93,7 +123,7 @@ export async function getServerSideProps(context) {
         return {
           name: song?.name || "Unknown Song",
           album: album,
-          debutShow: debutShow, // Corrected this line
+          debutShow: debutShow,
           currentShow: showId,
         };
       })
