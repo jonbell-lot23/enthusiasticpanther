7eb7ccb65b6dedf8c95550e98d2d2117624b9ec1
Jon Bell <github@lot23.com>
2024-10-13 10:18:48 +1300

    Some improvements

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ import Head from "next/head"; // Import Head from next/head
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
+      <>
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
         />
-      </Head>
-      <Header /> {/* Include the Header component */}
+      </>
+      <Header />
       <Component {...pageProps} />
     </>
   );
